6983419c29c180eb6442e2cf354e3a42e8c3a8f7
sahariardev <rifatsahariar@gmail.com>
2024-10-03 23:37:11 +0600

    Initialized frontend and added add rule UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,135 @@
+import {useState} from "react";
+
+function App() {
+
+    const [rules, setRules] = useState([{url: '', host: '', id: Date.now()}]);
+    const [featurName, setFeatureName] = useState('');
+
+    const handleAddNewRule = () => {
+        const newRules = [...rules];
+        newRules.push({url: '', host: '', id: Date.now()});
+        setRules(newRules);
+    }
+
+    const handleRemoveRule = (id) => {
+        const newRules = rules.filter((rule, i) => rule.id !== id).map((rule => {
+            return {
+                url: rule.url,
+                host: rule.host,
+                id: rule.id
+            }
+        }));
+        setRules(newRules);
+    }
+    const handleUrlChange = (index, event) => {
+        const newRules = [...rules];
+
+        newRules[index].url = event.value;
+
+        setRules(newRules);
+    }
+
+    const handleHostChange = (index, event) => {
+        const newRules = [...rules];
+
+        newRules[index].host = event.value;
+
+        setRules(newRules);
+    }
+
+    const generateRulesSection = () => {
+        return (
+            <div className="ruleContainer w-full">
+                {
+                    rules.map((rule, index) => ruleFormSection(rule, index))
+                }
+            </div>
+        )
+    }
+    const ruleFormSection = (rule, index) => {
+        return (
+            <div className="flex flex-wrap" key={index}>
+                <div className="p-2 w-1/4">
+                    <div className="relative">
+                        <input type="text" id="url" name="url"
+                               placeholder="URL Matcher"
+                               value={rule.url}
+                               onChange={(e) => handleUrlChange(index, e)}
+                               className="w-full bg-gray-100 bg-opacity-50 rounded
+                                           border border-gray-300 focus:border-indigo-500 focus:bg-white
+                                           focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700
+                                           py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
+                </div>
+                <div className="p-2 w-2/4">
+                    <div className="relative">
+                        <input type="text" id="host" name="host"
+                               placeholder="Host"
+                               value={rule.host}
+                               onChange={(e) => handleHostChange(index, e)}
+                               className="w-full bg-gray-100 bg-opacity-50
+                                           rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2
+                                           focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3
+                                           leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
+                </div>
+                <div className="p-2 w-1/4">
+                    <div className="relative">
+                        <button
+                            onClick={() => handleRemoveRule(rule.id)}
+                            className="flex mx-auto text-white bg-indigo-500 border-0 py-2
+                                 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Remove
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <section className="text-gray-600 body-font relative">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-12">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Config</h1>
+                        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Rule Order decides the priority</p>
+                    </div>
+                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                        <div className="flex flex-wrap -m-2">
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <input type="text" id="featurName" name="featurName"
+                                           placeholder="Feature Name"
+                                           value={featurName}
+                                           onChange={(e) => setFeatureName(e.target.value)}
+                                           className="w-full bg-gray-100 bg-opacity-50 rounded
+                                           border border-gray-300 focus:border-indigo-500 focus:bg-white
+                                           focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700
+                                           py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                                </div>
+                            </div>
+
+                            {generateRulesSection()}
+
+                            <div className="p-2 w-0.5/4">
+                                <button
+                                    onClick={(e) => handleAddNewRule()}
+                                    className="flex text-white bg-indigo-500 border-0 py-2
+                                 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add Rule
+                                </button>
+                            </div>
+
+                            <div className="p-2 w-0.5/4">
+                                <button className="flex text-white bg-indigo-500 border-0 py-2
+                                 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Save
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // Adjust if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
