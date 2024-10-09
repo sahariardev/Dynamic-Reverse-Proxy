1b1a23e2c75faac8ce96d962b8dffa90ac8f1351
sahariardev <rifatsahariar@gmail.com>
2024-10-09 21:56:25 +0600

    Added config to read cert and key path from env

--- backend/src/main.js
+++ backend/src/main.js
@@ -8,17 +8,19 @@ dotenv.config();
 
 const REVERSE_PROXY_SERVER_PORT = process.env.REVERSE_PROXY_SERVER_PORT || 443;
 const REVERSE_PROXY_CONFIG_SERVER_PORT = process.env.REVERSE_PROXY_CONFIG_SERVER_PORT || 441;
+const PATH_TO_CERT = process.env.PATH_TO_CERT;
+const PATH_TO_KEY = process.env.PATH_TO_KEY;
 
 https.createServer({
-    cert: fs.readFileSync('cert.pem'),
-    key: fs.readFileSync('key.pem')
+    cert: fs.readFileSync(PATH_TO_CERT),
+    key: fs.readFileSync(PATH_TO_KEY)
 }, reverserProxy).listen(REVERSE_PROXY_SERVER_PORT, () => {
     console.log(`Reverse proxy Server started on port ${REVERSE_PROXY_SERVER_PORT}`)
 });
 
 https.createServer({
-    cert: fs.readFileSync('cert.pem'),
-    key: fs.readFileSync('key.pem')
+    cert: fs.readFileSync(PATH_TO_CERT),
+    key: fs.readFileSync(PATH_TO_KEY)
 }, configServer).listen(REVERSE_PROXY_CONFIG_SERVER_PORT, () => {
     console.log(`Config Server started on port ${REVERSE_PROXY_CONFIG_SERVER_PORT}`)
 });
